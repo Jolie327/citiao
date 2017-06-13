2609d44b8b949ba3225e0db3f32e6b3e57347f52
Jolie327 <978494612@qq.com>
2017-06-13 09:16:54 +0800

    'n'

--- src/js/page/njl.js
+++ src/js/page/njl.js
@@ -1,3 +1,29 @@
 /**
  * Created by admin on 2017/6/10.
  */
+var $ = require("../lib/jquery-3.1.1");
+require("../lib/rem");
+    var page = {
+        init:function () {
+            this.clickMenu();
+        },
+        //点击目录时，遮罩层目录出现
+        clickMenu:function () {
+            $(".menu").click(function () {
+                $(".njl-shadow-main").show();
+            });
+            $(".shadow-left-list").click(function () {
+                $(".njl-shadow-main").hide();
+            })
+            // 点击目录列表：
+            $(".shadow-list-item").click(function () {
+                $(".njl-shadow-main").hide();
+                $(this).addClass("here").siblings().removeClass("here");
+                var index = $(this).index();
+                // var para = $(this).data("anchor");
+                $(".main-list-flow>a").removeClass().eq(index).addClass("anchor");
+                // $(".njl-main-citiao").css("margin-top","44px");
+            })
+        }
+    };
+    page.init();
